57a6464ac81bf5d43ebcd18f8b1ae5dff0a0b0bc
SayrexRR <sashazinchenko2000@gmail.com>
2023-07-28 18:50:06 +0300

    Добавьте файлы проекта.

---
+++ QuadraticEquation/Equation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuadraticEquation
+{
+    public class Equation
+    {
+        public double A { get; private set; }
+        public double B { get; private set; }
+        public double C { get; private set; }
+
+        public Equation()
+        {
+                
+        }
+
+        public Equation(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public string GetQuadraticEquation()
+        {
+            string x1, x2;
+            double discriminant = Math.Pow(B, 2) - 4 * A * C;
+
+            if (discriminant > 0)
+            {
+                x1 = ((-B + Math.Sqrt(discriminant)) / 2 * A).ToString();
+                x2 = ((-B - Math.Sqrt(discriminant)) / 2 * A).ToString();
+
+                return $"x1 = {x1}; x2 = {x2}";
+            }
+            else if (discriminant == 0)
+            {
+                x1 = ((-B + Math.Sqrt(discriminant)) / 2 * A).ToString();
+
+                return $"x = {x1}";
+            }
+            else
+            {
+                return "Discriminant < 0. Equation has no real roots";
+            }
+        }
+    }
+}
---
+++ QuadraticEquation/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace QuadraticEquation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input A");
+            double a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input B");
+            double b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Input C");
+            double c = double.Parse(Console.ReadLine());
+
+            Equation equation = new(a, b, c);
+
+            string result = equation.GetQuadraticEquation();
+
+            Console.WriteLine(result);
+
+            Console.WriteLine($"A = {equation.A}, B = {equation.B}, C = {equation.C}");
+        }
+    }
+}
