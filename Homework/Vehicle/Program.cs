400b6036d342ac750eb610bd61d8b742d149780c
Oleksandr <sashazinchenko2000@gmail.com>
2023-08-06 23:42:02 +0300

    Added Vehicle project

---
+++ Homework/Vehicle/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Vehicle
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SportCar sportCar = new("Subaru", "BRZ", 2022);
+            Truck truck = new("Renault Trucks", "T High", 2021);
+
+            sportCar.Drive();
+            sportCar.Stop();
+            sportCar.RecycleToMetal();
+
+            Console.WriteLine();
+
+            truck.Drive();
+            truck.Stop();
+            truck.LoadCargo();
+            truck.RecycleToMetal();
+        }
+    }
+}
