eaf44c3c70c38d45db7d4e6c779b53989af713fb
Oleksandr <sashazinchenko2000@gmail.com>
2023-08-14 15:32:22 +0300

    Added homework

---
+++ WhetherTemperatureInfo/Hydromecenter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WhetherTemperatureInfo
+{
+    class Hydromecenter
+    {
+        private TemperatureInfo[] temperatureArray;
+        private int temperatureIndex;
+
+        public Hydromecenter()
+        {
+            temperatureArray = new TemperatureInfo[20];
+            temperatureIndex = 0;
+        }
+
+        public void AddWeather(TemperatureInfo temperatureInfo)
+        {
+            if (temperatureIndex < temperatureArray.Length)
+            {
+                temperatureArray[temperatureIndex] = temperatureInfo;
+                temperatureIndex++;
+                return;
+            }
+
+            if (temperatureIndex > temperatureArray.Length)
+            {
+                Array.Resize(ref temperatureArray, temperatureArray.Length + 20);
+                AddWeather(temperatureInfo);
+            }
+        }
+
+        private TemperatureInfo GetTemperatureByDate(string date)
+        {
+            foreach (var temperature in temperatureArray)
+            {
+                if (temperature.Date == date)
+                {
+                    return temperature;
+                }
+            }
+
+            return new TemperatureInfo("00.00.0000", 0);
+        }
+
+        private TemperatureInfo[] GetTemperatureByPeriod(string start, string end)
+        {
+            TemperatureInfo[] result = new TemperatureInfo[temperatureArray.Length]; ;
+            int i = 0;
+
+            foreach (var temp in temperatureArray)
+            {
+                if (string.Compare(temp.Date, start) >= 0 && string.Compare(temp.Date, end) <= 0)
+                {
+                    result[i++] = temp;
+                }
+            }
+
+            var period = result.Where(t => t.Date != null).ToArray();
+
+            return period;
+        }
+
+        public TemperatureInfo this[string date]
+        {
+            get { return GetTemperatureByDate(date); }
+        }
+
+        public TemperatureInfo[] this[string start, string end]
+        {
+            get { return GetTemperatureByPeriod(start, end); }
+        }
+    }
+}
---
+++ WhetherTemperatureInfo/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace WhetherTemperatureInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hydromecenter hydromecenter = new Hydromecenter();
+
+            hydromecenter.AddWeather(new TemperatureInfo("14.08.2023", 27));
+            hydromecenter.AddWeather(new TemperatureInfo("15.08.2023", 29));
+            hydromecenter.AddWeather(new TemperatureInfo("16.08.2023", 28));
+            hydromecenter.AddWeather(new TemperatureInfo("17.08.2023", 27));
+            hydromecenter.AddWeather(new TemperatureInfo("18.08.2023", 29));
+            hydromecenter.AddWeather(new TemperatureInfo("19.08.2023", 30));
+            hydromecenter.AddWeather(new TemperatureInfo("20.08.2023", 28));
+
+
+            hydromecenter["15.08.2023"].GetInfo();
+
+            Console.WriteLine();
+
+            TemperatureInfo[] tempPeriod = hydromecenter["16.08.2023", "19.08.2023"];
+
+            foreach (var temp in tempPeriod)
+            {
+                temp.GetInfo();
+            }
+        }
+    }
+}
