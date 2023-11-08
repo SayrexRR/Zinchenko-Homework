825c2de2adc3b1e0f6b230059841222db3907968
Oleksandr <sashazinchenko2000@gmail.com>
2023-11-08 15:46:02 +0200

    EF Core project

---
+++ EFCoreProject/DBRepositories/DBRepository.cs
@@ -0,0 +1,29 @@
+﻿using EFCoreProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCoreProject.DBRepositories
+{
+    public class DBRepository
+    {
+        public static (double, double) GetProductSales(string productName, int month)
+        {
+            using (var context = new TestContext())
+            {
+                var productSales = from order in context.Orders
+                                   join orderPosition in context.OrderPositions on order.Id equals orderPosition.OrderId
+                                   join product in context.Products on orderPosition.ProductId equals product.Id
+                                   where product.Name == productName && order.Date.Month == month
+                                   select new { Qty = orderPosition.Qty, Total = orderPosition.Qty * product.Price };
+
+                var totalQty = productSales.Sum(p => p.Qty);
+                var totalSum = productSales.Sum(p => p.Total);
+
+                return (totalQty, (double)totalSum);
+            }
+        }
+    }
+}
---
+++ EFCoreProject/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EFCoreProject.Models;
+
+public partial class Product
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal? Price { get; set; }
+
+    public string? Description { get; set; }
+
+    public int? SupplierId { get; set; }
+
+    public virtual ICollection<OrderPosition> OrderPositions { get; set; } = new List<OrderPosition>();
+
+    public virtual ICollection<ProductInventory> ProductInventories { get; set; } = new List<ProductInventory>();
+
+    public virtual Brand? Supplier { get; set; }
+}
