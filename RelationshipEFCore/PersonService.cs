e29dcfa4a35610ceecddf95c2900626c2a45cc5d
Oleksandr <sashazinchenko2000@gmail.com>
2023-11-17 15:25:05 +0200

    Added service class and methods for display informations

---
+++ RelationshipEFCore/PersonService.cs
@@ -0,0 +1,27 @@
+﻿using RelationshipEFCore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RelationshipEFCore
+{
+    public static class PersonService
+    {
+        public static List<Employee> GetEmployeesByDepartmentId(int id)
+        {
+            return Repository.GetEmployeesByDepartmentId(id);
+        }
+
+        public static Employee GetEmployeeById(int id)
+        {
+            return Repository.GetEmployeeById(id);
+        }
+
+        public static List<Person> GetPersonsBornAfter(DateTime date)
+        {
+            return Repository.GetPersonsBornAfter(date);
+        }
+    }
+}
--- RelationshipEFCore/Program.cs
+++ RelationshipEFCore/Program.cs
@@ -57,21 +57,35 @@ namespace RelationshipEFCore
             }
             #endregion
 
+            PrintEmployeesByDepartment(2);
 
-            var employes = Repository.GetEmployeesByDepartmentId(1);
+            PrintEmployeeInfo(6);
+
+            PrintPersonsBornAfter(new DateTime(1999, 1, 1));
+        }
+
+        public static void PrintEmployeesByDepartment(int id)
+        {
+            var employes = PersonService.GetEmployeesByDepartmentId(id);
 
             foreach (var employe in employes)
             {
                 Console.WriteLine($"{employe.FirstName} {employe.Department.Name} {employe.Salary}");
             }
             Console.WriteLine(new string('-', 50));
+        }
 
-            var employee = Repository.GetEmployeeById(3);
+        public static void PrintEmployeeInfo(int id)
+        {
+            var employee = PersonService.GetEmployeeById(id);
 
             Console.WriteLine($"Name: {employee.FirstName} {employee.LastName}, Salary: {employee.Salary}, Department: {employee.Department.Name}");
             Console.WriteLine(new string('-', 50));
+        }
 
-            var personsBorn = Repository.GetPersonsBornAfter(new DateTime(2000, 1, 1));
+        public static void PrintPersonsBornAfter(DateTime date)
+        {
+            var personsBorn = PersonService.GetPersonsBornAfter(date);
 
             foreach (var person in personsBorn)
             {
