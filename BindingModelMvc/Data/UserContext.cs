0eb793693efbaef2b103727afbd819a08c44038b
Oleksandr <sashazinchenko2000@gmail.com>
2023-12-04 23:21:09 +0200

    Added project with model binding

---
+++ BindingModelMvc/Data/UserContext.cs
@@ -0,0 +1,15 @@
+﻿using BindingModelMvc.DataLayer.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BindingModelMvc.DataLayer.Data
+{
+    public class UserContext : DbContext
+    {
+        public UserContext(DbContextOptions<UserContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ BindingModelMvc/Models/UserModel.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BindingModelMvc.BusinessLayer.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+
+        [Required]
+        public string UserName { get; set; }
+
+        [EmailAddress]
+        public string? Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
