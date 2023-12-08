9434f17873af305cb6fbe0a6085cbe6a740a4c9c
Oleksandr <sashazinchenko2000@gmail.com>
2023-12-08 12:43:27 +0200

    Add validation

--- BindingModelMvc/Models/UserModel.cs
+++ BindingModelMvc/Models/UserModel.cs
@@ -7,12 +7,17 @@ namespace BindingModelMvc.BusinessLayer.Models
         public int UserId { get; set; }
 
         [Required]
+        [StringLength(75)]
+        [MinLength(3)]
+        [RegularExpression(@"^[a-zA-Z0-9\S]*$")]
         public string UserName { get; set; }
 
         [EmailAddress]
+        [MinLength(5)]
         public string? Email { get; set; }
 
         [Required]
+        [MinLength(6)]
         public string Password { get; set; }
     }
 }
